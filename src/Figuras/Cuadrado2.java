22a5c1a997536a7a84c75258691545f17b8cb591
juanmanuelrosalesscontreras@gmail.com <JUAN@DESKTOP-MMJTMFI>
2023-04-19 16:25:24 -0500

    Proyecto de dibujo de figuras usando clase abstracta y herencia Class Cuadrado1 Funcional Class Triangulo1 Funcional Class Cuadrado2 Funcional

---
+++ src/Figuras/Cuadrado2.java
@@ -0,0 +1,26 @@
+package Figuras;
+
+public class Cuadrado2 extends Figuras{
+    
+    @Override
+    public void imprimir(){
+        for(int i = 0; i <= n; i++){
+            for(int j = 0; j <= n; j++){
+                
+                if(i == 0 || i == n-1){
+                    System.out.print("*");
+                } else {
+                    if(j == 0 || j == n-1){
+                        System.out.print("*");
+                    } else {
+                        System.out.println(" ");
+                    }
+                }
+            }
+            
+            System.out.println("");
+            
+        }
+    }
+    
+}
--- test/TestFiguras.java
+++ test/TestFiguras.java
@@ -1,20 +1,28 @@
 import Figuras.Cuadrado1;
 import Figuras.Triangulo1;
 import Figuras.Triangulo2;
+import Figuras.Cuadrado2;
 
 public class TestFiguras {
     public static void main(String []args){
         Cuadrado1 c1 = new Cuadrado1();
         Triangulo1 t1 = new Triangulo1();
-        Triangulo2 t2 = new Triangulo2();
+        //Triangulo2 t2 = new Triangulo2();
+        Cuadrado2 c2 = new Cuadrado2();
         
         c1.setN(4);
+        System.out.println("Cuadrado1-------------");
         c1.imprimir();
         
         t1.setN(4);
+        System.out.println("Triangulo1-------------");
         t1.imprimir();
         
-        t2.setN(4);
-        t2.imprimir();        
+        c2.setN(4);
+        System.out.println("Cuadrado2-------------");
+        c2.imprimir();
+        
+       // t2.setN(4);
+       // t2.imprimir();        
     }
 }
